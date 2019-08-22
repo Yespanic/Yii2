c6abfb9ca23bd36588563dfce96c95c8da40fd05
Yespanic <yespanic@gmail.com>
2019-08-22 11:34:54 +0300

    init

---
+++ models/Articles.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+
+/**
+ * This is the model class for table "articles".
+ *
+ * @property int $id
+ * @property string $title
+ * @property string $content
+ * @property string $date
+ */
+class Articles extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'articles';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['title', 'content'], 'required'],
+            [['content'], 'string'],
+            [['date'], 'safe'],
+            [['title'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'title' => 'Title',
+            'content' => 'Content',
+            'date' => 'Date',
+        ];
+    }
+}
---
+++ views/site/index.php
@@ -0,0 +1,14 @@
+<?php
+
+/* @var $this yii\web\View */
+/* @var $articles */
+
+
+$this->title = 'Блог';
+?>
+<div class="site-index">
+
+    <?php print_r($articles); ?>
+
+
+</div>
